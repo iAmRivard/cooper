fd6c753723296d5c8ca5456dffbf934f681256a2
Diego Garcia <diego350z1@gmail.com>
2022-11-02 16:39:34 -0600

    Update create credito

--- app/Models/CrtPlanPagoDet.php
+++ app/Models/CrtPlanPagoDet.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class CrtPlanPagoDet extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'plan_pago_id',
+        'credito_id',
+        'socio_id',
+        'user_id',
+        'nro_cuota',
+        'cuota',
+        'interes',
+        'interes_acumulado',
+        'total',
+        'cuota_capital',
+        'saldo',
+        'capital_amortizado',
+        'fecha_programada',
+        'estado',
+    ];
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,14 +3,12 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
             @if (Auth::user()->rol == 'socio')
-
             {{-- Cuentas de ahorro --}}
             <div>
                 <div class="mx-4 my-2">
@@ -23,50 +21,35 @@
                 <div class="card w-96 sm:w-1/2 mx-4 my-2 bg-base-100 shadow-xl">
                     <div class="card-body">
                         <h2 class="card-title">${{ $cuenta->saldo_actual }}</h2>
-                        <p> 
-                            <a href="{{ route('ver.cuenta', $cuenta->id) }}" class="a-link">{{ $cuenta->tipoCuenta->nombre }} </a> 
+                        <p>
+                            <a href="{{ route('ver.cuenta', $cuenta->id) }}" class="a-link">{{ $cuenta->tipoCuenta->nombre }} </a>
                         </p>
                     </div>
                 </div>
                 @endforeach
                 </div>
             </div>
-            {{-- Cresitos --}}
-            @if (count($socio->creditos) >= 1)
-            <div>
-                <div class="mx-4 my-2">
-                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                        Creditos
-                    </h2>
-                </div>
-                <div class="flex flex-col sm:flex-row">
-                @foreach ($socio->creditos as $credito)
-                <div class="card w-96 sm:w-1/2 mx-4 my-2 bg-base-100 shadow-xl">
-                    <div class="card-body">
-                        <h2 class="card-title">${{ $credito->monto }}</h2>
-                        <p>{{ $credito->tipoCredito->nombre }}</p>
+                {{-- Cresitos --}}
+                @if (count($socio->creditos) >= 1)
+                <div>
+                    <div class="mx-4 my-2">
+                        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                            Creditos
+                        </h2>
+                    </div>
+                    <div class="flex flex-col sm:flex-row">
+                        @foreach ($socio->creditos as $credito)
+                        <div class="card w-96 sm:w-1/2 mx-4 my-2 bg-base-100 shadow-xl">
+                            <div class="card-body">
+                                <h2 class="card-title">${{ $credito->monto }}</h2>
+                                <p>{{ $credito->tipoCredito->nombre }}</p>
+                            </div>
+                        </div>
+                        @endforeach
                     </div>
                 </div>
-                @endforeach
-                </div>
-            </div>
+                @endif
             @endif
-
-            @endif
-
-
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-
-
-
-                {{-- hoals --}}
-                {{-- <x-jet-welcome /> --}}
-            </div>
-
-
-            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-
-
             </div>
         </div>
     </div>
