d83d63cc3708a7d188f329490ab9015b2a61afd4
Diego Garcia <diego350z1@gmail.com>
2022-11-10 07:31:01 -0600

    Update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,52 +8,57 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            @if (Auth::user()->rol == 'socio')
-            {{-- Cuentas de ahorro --}}
-            <div>
-                <div class="mx-4 my-2">
-                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                        Cuentas de ahorro
-                    </h2>
-                </div>
-                <div class="flex flex-col sm:flex-row">
-                @foreach ($socio->cuentas as $cuenta)
-                <a href="{{ route('cuenta.detalle', $cuenta->id) }}">
-                    <div class="card w-96 sm:w-1/2 mx-4 my-2 bg-base-100 shadow-xl">
-                        <div class="card-body">
-                            <h2 class="card-title">${{ $cuenta->saldo_actual }}</h2>
-                            <p>
-                                <a href="{{ route('ver.cuenta', $cuenta->id) }}" class="a-link">{{ $cuenta->tipoCuenta->nombre }} </a>
-                            </p>
-                        </div>
-                    </div>
-                </a>
-                @endforeach
-                </div>
-            </div>
-                {{-- Cresitos --}}
-                @if (count($socio->creditos) >= 1)
-                <div>
-                    <div class="mx-4 my-2">
-                        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                            Creditos
-                        </h2>
-                    </div>
-                    <div class="flex flex-col sm:flex-row">
-                        @foreach ($socio->creditos as $credito)
-                        <a href="{{ route('credito.detalle', $credito->id) }}">
-                            <div class="card w-96 sm:w-1/2 mx-4 my-2 bg-base-100 shadow-xl">
-                                <div class="card-body">
-                                    <h2 class="card-title">${{ $credito->monto }}</h2>
-                                    <p>{{ $credito->tipoCredito->nombre }}</p>
-                                </div>
+                @if (Auth::user()->rol == 'socio')
+                    <div class="sm:flex sm:justify-around flex flex-col items-center justify-center">
+                        {{-- Cuentas de ahorro --}}
+                        <div class="sm:w-full">
+                            <div class="mx-4 my-2">
+                                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                                    Cuentas de ahorro
+                                </h2>
                             </div>
-                        </a>
-                        @endforeach
+                            <div class="flex sm:flex-row lg:flex-col ">
+                                @foreach ($socio->cuentas as $cuenta)
+                                <a href="{{ route('cuenta.detalle', $cuenta->id) }}" class="sm:w-full">
+                                    <div class="card w-96 sm:w-1/2 mx-4 my-2 bg-base-100 shadow-xl">
+                                        <div class="card-body">
+                                            <h2 class="card-title">${{ $cuenta->saldo_actual }}</h2>
+                                            <p>
+                                                {{ $cuenta->tipoCuenta->nombre }}
+                                                {{-- <a href="{{ route('ver.cuenta', $cuenta->id) }}" class="a-link"> </a> --}}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </a>
+                                @endforeach
+                            </div>
+                        </div>
+                        {{-- Cresitos --}}
+                        @if (count($socio->creditos) >= 1)
+                        <div class="sm:w-full">
+                            <div class="mx-4 my-2">
+                                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                                    Creditos
+                                </h2>
+                            </div>
+                            <div class="sm:flex flex-row">
+                                @foreach ($socio->creditos as $credito)
+                                <a href="{{ route('credito.detalle', $credito->id) }}" class="sm:w-full">
+                                    <div class="card w-96 sm:w-1/2 mx-4 my-2 bg-base-100 shadow-xl">
+                                        <div class="card-body">
+                                            <h2 class="card-title">${{ $credito->monto }}</h2>
+                                            <p>
+                                                {{ $credito->tipoCredito->nombre }}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </a>
+                                @endforeach
+                            </div>
+                        </div>
+                        @endif
                     </div>
-                </div>
                 @endif
-            @endif
             </div>
         </div>
     </div>
