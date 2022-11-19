7dceeabfeb3249a86beb10d0659744d32a7ca116
Diego Garcia <diego350z1@gmail.com>
2022-11-19 14:33:37 -0600

    update margin able cuentas

--- resources/views/livewire/cuentas/cuentas.blade.php
+++ resources/views/livewire/cuentas/cuentas.blade.php
@@ -66,7 +66,7 @@
                         </table>
                     </div>
                 </div>
-                <div class="px-6 py-3 flex justify-items-center justify-around">
+                <div class="m-4">
                     {{$cuentas->links()}}
                 </div>
             </div>
