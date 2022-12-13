47ac35ccb12dd37820673b41b71ec9dd15c20d0b
Diego Garcia <diego350z1@gmail.com>
2022-12-13 07:39:10 -0600

    update view ver-credito

--- resources/views/livewire/ver-credito.blade.php
+++ resources/views/livewire/ver-credito.blade.php
@@ -1,12 +1,16 @@
 <div>
+    <x-slot name="header">
+        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+            @livewire('creditos.activar-desactivar', ['credito' => $credito])
+            {{-- @if (count($movimientos) == 1)
+                <label for="my-modal-5" class="open-moda-lable">open modal</label>
+            @endif --}}
+        </div>
+    </x-slot>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                {{-- <a href="{{route('cuenta.cuenta', $cuenta)}}" class="a-link">
-                    Imprimir
-                </a> --}}
-                {{-- <x-jet-welcome /> --}}
                 <div class="flex justify-center mb-4 mt-4">
                     <div class="stats shadow">
                         <div class="stat">
@@ -32,13 +36,16 @@
                             <div class="stat-title">Tasa Interés</div>
                             <div class="stat-value text-rose-900"> {{ $credito->porcentaje_interes}}%</div>
                         </div>
-                       
+
                         <div class="stat">
                             <div class="stat-value">{{$credito->socio->nombres}}</div>
                             <div class="stat-title">{{$credito->socio->apellidos}}</div>
                         </div>
 
-            
+                        <div class="stat">
+                            <div class="stat-value @if($credito->estado == 0) text-rose-900 @endif" >{{ $credito->estado == 1 ? 'Activado' : 'Desactivado' }}</div>
+                            <div class="stat-title">Estado</div>
+                        </div>
                     </div>
                 </div>
                 <div class="flex justify-center mb-4">
@@ -50,7 +57,6 @@
                                 <th>Concepto</th>
                                 <th>Monto</th>
                                 <th>Fecha</th>
-                                
                             </tr>
                         </thead>
                         <tbody>
@@ -65,7 +71,6 @@
                                     </span>
                                 </td>
                                 <td class="text-center">{{ $movimiento->created_at }}</td>
-                                
                             </tr>
                             @endforeach
                         </tbody>
@@ -73,7 +78,46 @@
                 </div>
                 {{$movimientos->links()}}
             </div>
+        </div>
+    </div>
 
+    <input type="checkbox" id="my-modal-5" class="modal-toggle" />
+    <div class="modal">
+        <div class="modal-box w-11/12 max-w-5xl">
+            <form action="" method="post" id="edit-credito">
+                <div class="flex gap-4">
+                    <div class="form-control w-full">
+                        <label class="label">
+                            <span class="label-text">monto</span>
+                        </label>
+                        <input type="text" placeholder="Monto del credito" class="input input-bordered w-full" value="{{ number_format($credito->monto, 2) }}" />
+                    </div>
+                    <div class="form-control w-full">
+                        <label class="label">
+                            <span class="label-text">Cuota Fija</span>
+                        </label>
+                        <input type="text" placeholder="Cuota fija" class="input input-bordered w-full" value="{{ number_format($credito->cuotaFija()->cuota_fija, 2) }}" />
+                    </div>
+                </div>
+                <div class="flex gap-4">
+                    <div class="form-control w-full">
+                        <label class="label">
+                            <span class="label-text">Periodo</span>
+                        </label>
+                        <input type="text" placeholder="Periodo en quincenas" class="input input-bordered w-full" value="{{ $credito->cantidad_cuotas }}" />
+                    </div>
+                    <div class="form-control w-full">
+                        <label class="label">
+                            <span class="label-text">Porcenaje</span>
+                        </label>
+                        <input type="text" placeholder="Porcentaje de interes" class="input input-bordered w-full" value="{{ number_format($credito->porcentaje_interes, 2) }}" />
+                    </div>
+                </div>
+            </form>
+            <div class="modal-action">
+                <label for="my-modal-5" class="btn">Cancelar!</label>
+                <button class="btn">Editar</button>
+            </div>
         </div>
     </div>
 </div>
