0cf1b1b3ecc03401f6d12cfb025f49356a8a0239
Diego Garcia <diego350z1@gmail.com>
2022-12-26 10:42:12 -0600

    delete laravel permisions

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,8 +10,6 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
-use Spatie\Permission\Traits\HasRoles;
-
 class User extends Authenticatable
 {
     use HasApiTokens;
@@ -19,7 +17,6 @@ class User extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
-    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
