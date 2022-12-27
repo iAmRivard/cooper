1f3db073c207141a12863c9ca4af255fb187a87b
Erick <erick.sgr10@gmail.com>
2022-12-27 14:57:27 -0600

    Actualizacion de menus

--- resources/views/partials/menu-reportes.blade.php
+++ resources/views/partials/menu-reportes.blade.php
@@ -1,6 +1,8 @@
+<!-- 
 <x-jet-dropdown-link href="{{ route('reporte.cierre-cuentas') }}">
     Cierre de cuentas
 </x-jet-dropdown-link>
+-->
 <x-jet-dropdown-link href="{{ route('reporte.cobro-cuotas') }}">
     Cobro cuotas Quincenales
 </x-jet-dropdown-link>
