255746d60a8756e30e0f18d53ced77ff3e8e48cb
Erick <erick.sgr10@gmail.com>
2023-05-10 15:55:23 -0600

    Actualizaciones visuales

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,60 +6,62 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                @if (Auth::user()->rol == 'socio')
-                    <div class="sm:flex sm:justify-around flex flex-col items-center justify-center">
-                        {{-- Cuentas de ahorro --}}
-                        <div class="sm:w-full">
-                            <div class="mx-4 my-2">
-                                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                                    Cuentas de ahorro
-                                </h2>
-                            </div>
-                            <div class="grid lg:grid-cols-2 grid-cols-1 gap-4 mb-4">
-                                @foreach ($socio->cuentas as $cuenta)
-                                <a href="{{ route('cuenta.detalle', $cuenta->id) }}" class="sm:w-full">
-                                    <div class="card w-100 mx-4 my-2 bg-base-100 shadow-xl">
-                                        <div class="card-body">
-                                            <h2 class="card-title">${{ $cuenta->saldo_actual }}</h2>
-                                            <p>
-                                                {{ $cuenta->tipoCuenta->nombre }}
-                                                {{-- <a href="{{ route('ver.cuenta', $cuenta->id) }}" class="a-link"> </a> --}}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </a>
-                                @endforeach
-                            </div>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            @if (Auth::user()->rol == 'socio')
+                <div class="sm:flex sm:justify-around flex flex-col items-center justify-center">
+                    {{-- Cuentas de ahorro --}}
+                    <div class="sm:w-full">
+                        <div class="mx-4 my-2">
+                            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                                Cuentas de ahorro
+                            </h2>
                         </div>
-                        {{-- Cresitos --}}
-                        @if (count($socio->creditos) >= 1)
-                        <div class="sm:w-full">
-                            <div class="mx-4 my-2">
-                                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                                    Creditos
-                                </h2>
-                            </div>
-                            <div class="grid lg:grid-cols-2 grid-cols-1 gap-4 mb-4">
-                                @foreach ($socio->creditos as $credito)
-                                <a href="{{ route('credito.detalle', $credito->id) }}" class="sm:w-full">
-                                    <div class="card w-100 mx-4 my-2 bg-base-100 shadow-xl">
-                                        <div class="card-body">
-                                            <h2 class="card-title">${{ $credito->monto }}</h2>
-                                            <p>
-                                                {{ $credito->tipoCredito->nombre }}
-                                            </p>
-                                        </div>
+                        <div class="grid lg:grid-cols-2 grid-cols-1 gap-4 mb-4">
+                            @foreach ($socio->cuentas as $cuenta)
+                            <a href="{{ route('cuenta.detalle', $cuenta->id) }}" class="sm:w-full">
+                                <div class="card w-100 sm:mx-4 sm:my-2 mx-2 my-1 bg-base-100 shadow-xl">
+                                    <div class="card-body">
+                                        <h2 class="text-lg sm:text-xl">${{ $cuenta->saldo_actual }}</h2>
+                                        <p class="text-sm sm:text-base">
+                                            {{ $cuenta->tipoCuenta->nombre }}
+                                        </p>
                                     </div>
-                                </a>
-                                @endforeach
-                            </div>
+                                </div>
+                            </a>
+                            @endforeach
                         </div>
-                        @endif
                     </div>
-                @endif
-            </div>
+                    {{-- Creditos --}}
+                    @if (count($socio->creditos) >= 1)
+                    <div class="sm:w-full">
+                        <div class="mx-4 my-2">
+                            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                                Creditos
+                            </h2>
+                        </div>
+                        <div class="grid lg:grid-cols-2 grid-cols-1 gap-4 mb-4">
+                            @foreach ($socio->creditos as $credito)
+                            <a href="{{ route('credito.detalle', $credito->id) }}" class="sm:w-full">
+                                <div class="card w-100 sm:mx-4 sm:my-2 mx-2 my-1 bg-base-100 shadow-xl">
+                                    <div class="card-body">
+                                        <h2 class="text-lg sm:text-xl">${{ $credito->monto }}</h2>
+                                        <p class="text-sm sm:text-base">
+                                            {{ $credito->tipoCredito->nombre }}
+                                        </p>
+                                    </div>
+                                </div>
+                            </a>
+                            @endforeach
+                        </div>
+                    </div>
+                    @endif
+                </div>
+            @endif
         </div>
     </div>
+</div>
+
+
+
 </x-app-layout>
--- resources/views/socio/cuenta.blade.php
+++ resources/views/socio/cuenta.blade.php
@@ -5,86 +5,108 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+    <div class="py-12 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-screen-xl mx-auto">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
 
-            <div class="flex justify-center mb-4 mt-4">
-                <div class="stats shadow">
-                    <div class="stat">
-                        <div class="stat-title">Numero de cuenta </div>
-                        <div class="stat-value text-primary">#{{$cuenta->id}}</div>
-                        <div class="stat-desc">{{$cuenta->tipoCuenta->nombre}}</div>
-                    </div>
-                    @if ($cuenta->tipoCuenta->plazo)
-                    <div class="stat">
-                        <div class="stat-title">Monto Apertura</div>
-                        <div class="stat-value text-sky-900">${{number_format($cuenta->monto_plazo)}}</div>
-                        <div class="stat-desc">MONTO INICIAL</div>
-                    </div>
-                    @endif
-                    <div class="stat">
-                        <div class="stat-title">Saldo a la fecha</div>
-                        <div class="stat-value text-green-900">${{number_format($cuenta->saldo_actual, 2)}}</div>
+            <div class="flex flex-col space-y-4 sm:space-y-0 sm:flex-row sm:space-x-4 mb-4">
+                <div class="flex-1 p-4 shadow rounded">
+                    <div class="stat-title">Numero de cuenta </div>
+                    <div class="stat-value text-primary">#{{$cuenta->id}}</div>
+                    <div class="stat-desc">{{$cuenta->tipoCuenta->nombre}}</div>
+                </div>
+                @if ($cuenta->tipoCuenta->plazo)
+                <div class="flex-1 p-4 shadow rounded">
+                    <div class="stat-title">Monto Apertura</div>
+                    <div class="stat-value text-sky-900">${{number_format($cuenta->monto_plazo)}}</div>
+                    <div class="stat-desc">MONTO INICIAL</div>
+                </div>
+                @endif
+                <div class="flex-1 p-4 shadow rounded">
+                    <div class="stat-title">Saldo a la fecha</div>
+                    <div class="stat-value text-green-900">${{number_format($cuenta->saldo_actual, 2)}}</div>
+                    <div class="stat-desc">
                         @if ($cuenta->tipoCuenta->plazo)
-                        <div class="stat-desc">MONTO INICIAL + INTERESES</div>
+                        MONTO INICIAL + INTERESES
                         @else
-                        <div class="stat-desc">MONTO INICIAL + ABONOS/INTERESES</div>
+                        MONTO INICIAL + ABONOS/INTERESES
                         @endif
                     </div>
-                    <div class="stat">
-                        <div class="stat-value">{{$cuenta->socio->nombres}}</div>
-                        <div class="stat-title">{{$cuenta->socio->apellidos}}</div>
+                </div>
+                @if ($cuenta->tipoCuenta->plazo)
+                <div class="flex-1 p-4 shadow rounded">
+                    <div class="stat-title">{{$cuenta->finalizado ? 'Finalizado' : 'Ejecución'}}</div>
+                    <div class="stat-value">{{ $cuenta->quincena_actual }}@if($cuenta->quincena_actual != null)/@else / @endif{{ $cuenta->cantidad_quincenas }}</div>
+                    <div class="stat-desc">CUOTAS</div>
+                </div>
+                @endif
+            </div>
+            <div class="flex flex-col overflow-x-auto">
+    @foreach($cuenta->mv as $detalle)
+        <div class="bg-white shadow overflow-hidden sm:rounded-lg my-2 cursor-pointer" onclick="openModal('modal{{ $detalle->id }}')">
+            <div class="px-4 py-5 sm:px-6 flex justify-between">
+                <div class="text-sm leading-6 font-medium text-gray-900">
+                <span class="text-sm @if($detalle->tipo->naturaleza == 1) text-green-600 @else text-red-600 @endif">
+                        ${{ number_format($detalle->monto, 2) }}
+                    </span>
+                    | {{ $detalle->tipo->nombre }} 
+               
+                </div>
+                <div class="text-sm text-gray-500">{{ date_format($detalle->created_at, 'd/m/Y') }}</div>
+            </div>
+        </div>
+        <div id="modal{{ $detalle->id }}" class="hidden fixed z-10 inset-0 overflow-y-auto">
+    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div class="sm:flex sm:items-start">
+                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+                            Detalles del movimiento
+                        </h3>
+                        <div class="mt-2">
+                            <p class="text-sm text-gray-500">
+                                <strong>Concepto:</strong> {{ $detalle->concepto }}<br>
+                                <strong>Monto:</strong> ${{ number_format($detalle->monto, 2) }}<br>
+                                <strong>Saldo a la fecha:</strong> ${{ number_format($detalle->saldo_fecha, 2) }}<br>
+                                <strong>Tipo:</strong> {{ $detalle->tipo->nombre }}<br>
+                                <strong>Fecha:</strong> {{ date_format($detalle->created_at, 'd/m/Y') }}
+                            </p>
+                        </div>
                     </div>
-
-                    @if ($cuenta->tipoCuenta->plazo)
-                    <div class="stat">
-                        <div class="stat-title">{{$cuenta->finalizado ? 'Finalizado' : 'Ejecución'}}</div>
-                        <div class="stat-value">{{ $cuenta->quincena_actual }}@if($cuenta->quincena_actual != null)/@else / @endif{{ $cuenta->cantidad_quincenas }}</div>
-                        <div class="stat-desc">CUOTAS</div>
-                    </div>
-                    @endif
                 </div>
             </div>
-
-            <div class="overflow-x-auto p-4">
-                <table class="table w-full">
-                    <!-- head -->
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Concepto</th>
-                            <th>Monto</th>
-                            <th>Saldo a la fecha</th>
-                            <th>Tipo</th>
-                            <th>Fecha</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($cuenta->mv as $detalle)
-                        <tr>
-                            <th>{{ $detalle->id }}</th>
-                            <td>{{ $detalle->concepto }}</td>
-                            <td class="@if($detalle->tipo->naturaleza == 1) text-green-600 @else text-red-600 @endif">
-                                $ {{ number_format($detalle->monto, 2) }}
-                            </td>
-                            <td>
-                                $ {{ number_format($detalle->saldo_fecha, 2) }}
-                            </td>
-                            <td>
-                                {{ $detalle->tipo->nombre }}
-                            </td>
-                            <td>
-                                {{ date_format($detalle->created_at, 'd/m/Y') }}
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-
-
+            <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" onclick="closeModal('modal{{ $detalle->id }}')">
+                    Cerrar
+                </button>
             </div>
         </div>
     </div>
+</div>
+
+    @endforeach
+
+</div>
+
+
+        </div>
+        
+    </div>
+    
+</div>
+
+
+<script>
+    function openModal(modalId) {
+        document.getElementById(modalId).classList.remove('hidden');
+    }
+
+    function closeModal(modalId) {
+        document.getElementById(modalId).classList.add('hidden');
+    }
+</script>
 </x-app-layout>
