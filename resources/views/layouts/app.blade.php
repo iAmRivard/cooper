ab13f27ac485b5f35cf30f6eae76c36f0bb5a7b9
Diego Garcia <diego350z1@gmail.com>
2023-08-07 20:32:41 -0600

    install fontawesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,53 +1,58 @@
 <!DOCTYPE html data-theme="light">
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        {{-- LIB externas Fontawesone Free --}}
-        <link rel="stylesheet" href="{{ asset('css/lib.css') }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://kit.fontawesome.com/2115fb84e0.js" crossorigin="anonymous"></script>
 
-        @livewireStyles
+    {{-- LIB externas Fontawesone Free --}}
+    <link rel="stylesheet" href="{{ asset('css/lib.css') }}">
 
-    </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+    @livewireStyles
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+</head>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+<body class="font-sans antialiased">
+    <x-jet-banner />
 
-        @stack('modals')
+    <div class="min-h-screen bg-gray-100">
+        @livewire('navigation-menu')
 
-        @stack('scripts')
+        <!-- Page Heading -->
+        @if (isset($header))
+        <header class="bg-white shadow">
+            <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+        @endif
 
-        @livewireScripts
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
 
-        <script>
-            Livewire.on('exito', function(message){
-                Swal.fire(
-                    message
-                )
-            })
-        </script>
-    </body>
+    @stack('modals')
+
+    @stack('scripts')
+
+    @livewireScripts
+
+    <script>
+        Livewire.on('exito', function(message){
+            Swal.fire(
+                message
+            )
+        })
+    </script>
+</body>
+
 </html>
