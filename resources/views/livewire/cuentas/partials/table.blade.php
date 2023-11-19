e23208ac7b96850671b0c0ca997f751a63c215d0
Diego Garcia <diego350z1@gmail.com>
2023-11-19 11:39:24 -0600

    Update title

--- resources/views/livewire/cuentas/partials/table.blade.php
+++ resources/views/livewire/cuentas/partials/table.blade.php
@@ -7,7 +7,7 @@
     <table class="table table-xs table-zebra">
         <thead>
             <tr>
-                <th># Empleado</th>
+                <th>Cod empleado</th>
                 <th># Socio</th>
                 <th>Socio</th>
                 <th># Cuenta</th>
@@ -22,38 +22,38 @@
         </thead>
         <tbody>
             @foreach ($cuentas as $cuenta)
-            <tr>
-                <td>{{ $cuenta->socio->codigo_empleado }}</td>
-                <td>{{ $cuenta->socio->numero_socio ?? 'N/A' }}</td>
-                <td>
-                    <div class="truncate">
-                        {{ $cuenta->socio->nombres . " " . $cuenta->socio->apellidos }}
-                    </div>
-                </td>
-                <td >
-                    <b>{{ $cuenta->id}}</b> - {{ $cuenta->no_cuenta}}
-                </td>
-                <td>
-                    <b>{{ $cuenta->tipoCuenta->nombre }}</b>
-                </td>
-                <td>
-                    {{ $cuenta->pago_quincenal != null ? '$'.$cuenta->pago_quincenal : 'N/A' }}
-                </td>
-                <td class="text-green">${{ $cuenta->saldo_actual }}</td>
-                <td class="font-bold text-center">
-                    <span @if ($cuenta->finalizado == 1) class="text-green-600" @else class="text-yellow-600" @endif >
-                        {{ $cuenta->finalizado ? 'Finalizado' : 'Ejecución' }}
-                    </span>
-                </td>
-                <td>{{ Carbon::parse($cuenta->created_at)->locale('es_ES')->format('d-m-Y') }}</td>
-                <td>{{ $cuenta->estado ? 'Activo' : 'Inactivo' }}</td>
+                <tr>
+                    <td>{{ $cuenta->socio->codigo_empleado }}</td>
+                    <td>{{ $cuenta->socio->numero_socio ?? 'N/A' }}</td>
+                    <td>
+                        <div class="truncate">
+                            {{ $cuenta->socio->nombres . " " . $cuenta->socio->apellidos }}
+                        </div>
+                    </td>
+                    <td >
+                        <b>{{ $cuenta->id}}</b> - {{ $cuenta->no_cuenta}}
+                    </td>
+                    <td>
+                        <b>{{ $cuenta->tipoCuenta->nombre }}</b>
+                    </td>
+                    <td>
+                        {{ $cuenta->pago_quincenal != null ? '$'.$cuenta->pago_quincenal : 'N/A' }}
+                    </td>
+                    <td class="text-green">${{ $cuenta->saldo_actual }}</td>
+                    <td class="font-bold text-center">
+                        <span @if ($cuenta->finalizado == 1) class="text-green-600" @else class="text-yellow-600" @endif >
+                            {{ $cuenta->finalizado ? 'Finalizado' : 'Ejecución' }}
+                        </span>
+                    </td>
+                    <td>{{ Carbon::parse($cuenta->created_at)->locale('es_ES')->format('d-m-Y') }}</td>
+                    <td>{{ $cuenta->estado ? 'Activo' : 'Inactivo' }}</td>
 
-                <td>
-                    <a href="{{ route('ver.cuenta', $cuenta->id) }}" class="text-sm btn btn-sm btn-primary">
-                        ver cuenta
-                    </a>
-                </td>
-            </tr>
+                    <td>
+                        <a href="{{ route('ver.cuenta', $cuenta->id) }}" class="text-sm btn btn-sm btn-primary">
+                            ver cuenta
+                        </a>
+                    </td>
+                </tr>
             @endforeach
         </tbody>
         <tfoot>
