b0dd22d1e3cb2ae20494161fb81ab69877e1e208
Diego Garcia <diego350z1@gmail.com>
2023-12-08 08:59:38 -0600

    update table

--- resources/views/livewire/socios/partials/table.blade.php
+++ resources/views/livewire/socios/partials/table.blade.php
@@ -1,38 +1,53 @@
-<table class="table table-fixed">
-    <thead>
-        <tr>
-            <th>Cod. Empleado</th>
-            <th># de Socio</th>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Empresa</th>
-            <th>DUI</th>
-            <th>Ap. Quincenal</th>
-            <th>&nbsp;</th>
-            <th>&nbsp;</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($socios as $item)
+<div class="overflow-x-auto">
+    <table class="table table-xs table-zebra">
+        <thead>
             <tr>
-                <td class="font-bold">{{ $item->codigo_empleado }}</td>
-                <td>{{ $item->numero_socio ?? 'No definido' }}</td>
-                <td>{{ $item->nombres}} </td>
-                <td>{{ $item->apellidos}}</td>
-                <td class="font-bold" >{{ $item->empresa->nombre }}</td>
-                <td>{{ $item->dui }}</td>
-                <td class="font-bold" >${{ $item->aportacion }}</td>
-                <td>
-                    <a class="cursor-pointer" wire:click="editar( {{$item}} )">
-                        <i class="fas fa-edit"></i>
-                    </a>
-                </td>
-                <td>
-                    <a href="{{route('ver.socio', $item)}}" class="font-semibold">
-                        Ver socio
-                    </a>
-                </td>
+                <th>Cod. Empleado</th>
+                <th># de Socio</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Empresa</th>
+                <th>DUI</th>
+                <th>Ap. Quincenal</th>
+                <th>&nbsp;</th>
+                <th>&nbsp;</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach ($socios as $item)
+                <tr>
+                    <td class="font-bold">{{ $item->codigo_empleado }}</td>
+                    <td>{{ $item->numero_socio ?? 'No definido' }}</td>
+                    <td>{{ $item->nombres}} </td>
+                    <td>{{ $item->apellidos}}</td>
+                    <td class="font-bold" >{{ $item->empresa->nombre }}</td>
+                    <td>{{ $item->dui }}</td>
+                    <td class="font-bold" >${{ $item->aportacion }}</td>
+                    <td>
+                        <a class="cursor-pointer" wire:click="editar( {{$item}} )">
+                            <i class="fas fa-edit"></i>
+                        </a>
+                    </td>
+                    <td>
+                        <a href="{{route('ver.socio', $item)}}" class="font-semibold">
+                            Ver socio
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>Cod. Empleado</th>
+                <th># de Socio</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Empresa</th>
+                <th>DUI</th>
+                <th>Ap. Quincenal</th>
+                <th>&nbsp;</th>
+                <th>&nbsp;</th>
+            </tr>
+        </tfoot>
+    </table>
+</div>
